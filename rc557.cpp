ee82933707502bff66ccac62d11c102b449b51e2
czp001 <30425821+czp001@users.noreply.github.com>
2019-11-15 16:29:20 +0800

    Create rc557.cpp

---
+++ rc557.cpp
@@ -0,0 +1,25 @@
+//test
+
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+int main(){
+    LL t, s, n, a, b;
+    LL mod = 1000000000;
+    n = 100000000000;
+    t = 0;
+    for (a = 1; a <= LL(sqrt(n / 3)); a++)
+    { 
+        if(a%1000==0)
+            cout << a << endl;
+        for (b = a; a * a + b * b + a * b <= n; b++)
+        {
+            if(__gcd(a,b)==1&&a*a+a*b-b*b>0){
+                t = t + 1;
+                s = s + a * a+ 3 * a * b+ b * b;
+                s %= mod;
+            }
+        }
+    }
+    cout << t << " ," << s << endl;
+}
